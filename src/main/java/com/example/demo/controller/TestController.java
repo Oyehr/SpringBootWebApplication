732cdd8f28eaf408f2285556a1e510301b8c98f3
Oyehr <16oeahr@163.com>
2019-01-09 11:22:22 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/test")
+    public String getCaseDecideView() {
+        return "test";
+    }
+
+}
